a34e1e83436ac3844e46e950f83e5342d90789ab
sleepcoder <rodrigo_tui_@hotmail.com>
2014-01-10 12:53:07 -0200

    Create campo.php

---
+++ prova-sexta/campo.php
@@ -0,0 +1,38 @@
+<br />
+<fieldset>
+<label>Nome do Produto:</label><input type="text" name="nome" />
+</fieldset>
+<br/>
+
+<fieldset>
+    <label>Produto Novo:</label><input type="checkbox" name="novo" />
+</fieldset>
+<br />
+
+<fieldset>
+    <label>Cor:</label> <br />
+    
+    <label>Azul</label>
+    <input type="radio" name="cor" value="azul"/> <br />
+    
+    <label>Verde</label>
+    <input type="radio" name="cor" value="verde" />  <br />
+    
+    <label>Vermelho</label>
+    <input type="radio" name="cor" value="vermelho" /> <br />
+    
+</fieldset>
+<br />
+<fieldset>
+        <label>Prazo de Entrega</label> <br /> <br />
+        <select name="prazo">  
+        <option>5 Dias</option>
+        <option>10 Dias</option>
+        <option>15 Dias</option>
+    </select>
+</fieldset>
+
+<fieldset>
+    <label>Detalhes do Produto:</label> <br />
+    <textarea name="detalhes" > </textarea>
+</fieldset>
