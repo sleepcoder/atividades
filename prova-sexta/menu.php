cf7ea86b8632293f2c14fade890d5df180b66d40
sleepcoder <rodrigo_tui_@hotmail.com>
2014-01-10 12:55:37 -0200

    Create menu.php

---
+++ prova-sexta/menu.php
@@ -0,0 +1,14 @@
+<link rel="stylesheet" type="text/css" href="estilos.css" >
+
+
+<div class="menu">
+    <div class="centro">
+     <ul>
+    <li><a href="index.php">Lista</a></li>
+    <li><a href="cadastrar_form.php">Cadastrar</a></li>
+    <li><a href="remover_form.php">Remover</a></li>
+    <li><a href="editar_form.php">Editar</a></li>
+        </ul>
+      <div>
+  </div>
+<div class="clear">&nbsp;</div>
